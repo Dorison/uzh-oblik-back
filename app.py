1133ac581d44a53a5c91a6a03fb472194c20cac0
AndriiKozak <andrii.kozak@gmail.com>
2023-11-10 11:25:49 +0200

    CI/CD

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-
+from flask import Flask, request, abort
+from http import HTTPStatus
 from user import user_manager, authenticate
 
 from flask_login import LoginManager
@@ -9,6 +9,7 @@ def load_user(user_id):
     return user_manager.get(user_id)
 
 app = Flask(__name__)
+# TODO implement more secure way.
 app.secret_key = '009e5686fbe6267253fa2c0acfae50f6c4b1e0ae3e12184b101d461f32e49b7e'
 login_manager.init_app(app)
 
@@ -23,15 +24,15 @@ def authenticate():
     user = user_manager.users_by_id(username)
     if authenticate(user, password):
 
-        return {'username': username, 'password': password}, 401
+        return {'username': username, 'password': password}, HTTPStatus.UNAUTHORIZED
     else:
-        return {'username': username, 'password': password}, 200
+        return {'username': username, 'password': password}, HTTPStatus.FOUND
 
 
 
 @app.route("/hello")
 def hello():
-    return "hello World"
+    return "hello CI/CD!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
