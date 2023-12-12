9033f3fb020ec2c25a2c6bccd584ca8d751857ed
AndriiKozak <andrii.kozak@gmail.com>
2023-12-12 14:48:55 +0200

    fixed add item

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def get_item(id):
 def add_stock(id):
     item = item_manager.get_by_id(id)
     # {"size": count}
-    logger.info(f"stock updated {request.json}")
+    logger.error(f"stock updated {request.json}")
     item_manager.add_stock(item, request.json)
 
 
